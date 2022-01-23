636817e2b2d02dbdefc9690ea14c511e70066871
gorotakeda <rugbytkg3@gmail.com>
2022-01-23 21:14:02 +0900

    userテーブル再作成

--- db/migrate/20220121022018_add_company_id_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddCompanyIdToUsers < ActiveRecord::Migration[7.0]
-  def change
-
-  end
-end
--- db/migrate/20220121022355_remove_company_id_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCompanyIdFromUsers < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :company_id, :integer
-  end
-end
