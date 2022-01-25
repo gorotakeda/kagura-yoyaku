3cb3de3b7c90e93b93e6e3cddc5962d89fb70e24
gorotakeda <rugbytkg3@gmail.com>
2022-01-25 09:45:57 +0900

    companies,reservationコントローラー作成

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,9 @@
+class ReservationsController < ApplicationController
+  def new
+    
+  end
+  
+  def create
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: "reservations#new"
+  resources :reservations , only: [:new, :create]
 end
